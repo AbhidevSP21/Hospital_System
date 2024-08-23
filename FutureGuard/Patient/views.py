c62a5e2cce31c0cbf1bcd5f6ef3c341b6ca53f0c
GokulManoj000 <gokul.manoj@msds.christuniversity.in>
2024-08-23 08:38:41 +0530

    new

--- FutureGuard/Patient/views.py
+++ FutureGuard/Patient/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login , logout
+from .models import user_details, interests, improvements,teching,feedback
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+def index(request):
+    return render(request,"Patient/Main/index.html")
