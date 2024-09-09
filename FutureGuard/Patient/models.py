5981a353aa0eaa9cd21d7fbff0d3321a099c7eac
GokulManoj000 <gokul.manoj@msds.christuniversity.in>
2024-09-09 13:49:08 +0530

    auth

--- FutureGuard/Patient/models.py
+++ FutureGuard/Patient/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# class user(User):
+#     ROLES = (
+#         ('patient','Patient'),
+#         ('doctor','Doctor'),
+#     )
+#     role=models.CharField(max_length=20,choices=ROLES,default='Patient')
+
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=15)
+    password = models.CharField(max_length=20)
+    role=models.CharField(max_length=20,choices=(
+        ('patient','Patient'),
+        ('doctor','Doctor'),
+    ),default='Patient')
--- FutureGuard/Patient/views.py
+++ FutureGuard/Patient/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from .models import PatientProfile
+from django.contrib.auth.hashers import make_password
+
 
 
 def index(request):
     return render(request,"Patient\Main\index.html",context={})
 
-def login(request) :
+def login_access(request) :
     return render(request, "Patient\Main\login.html",context={})
 
 def doctorlist(request) :
@@ -16,4 +21,51 @@ def contact(request) :
     return render(request, "Patient\Main\contact.html",context={})
 def about(request) :
     return render(request, "Patient/Main/about.html",context={})
+def userprofile(request) :
+    return render(request, "Patient/Main/userprofile.html",context={})
 
+def patient_register(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        phone_no=request.POST['phone_no']
+        password=request.POST['password']
+        role='Patient'
+        user=User.objects.create_user(username=email)
+        user.set_password(password)
+        user.save()
+        PatientProfile.objects.create(user=user,email=email,name=name,phone_no=phone_no,role=role)
+
+        login(request,user)
+
+        return redirect('login')
+    return render(request,"{% url 'user_login' %}")
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        user = authenticate(request,username=username,password=password)
+        if user is not None and user.is_active:
+            # Redirect based on user role
+            if user.is_superuser == False and user.is_staff == True:
+                login(request, user)
+                return redirect('index')  # redirect to the 'index' view
+            
+            elif user.is_superuser == False and user.is_staff == False:
+                login(request, user)
+                return redirect('userprofile')  # redirect to 'userprofile' view
+        
+        else:
+            # Wrong credentials or inactive user
+            msg = "Wrong credentials. Please try again!"
+            return render(request, 'Patient/Main/login.html', {'msg': msg})
+    
+    # For GET request or if not logged in, show the login page
+    return render(request, 'Patient/Main/login.html')
+    
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
