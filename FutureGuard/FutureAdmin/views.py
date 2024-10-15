a7b80d0dec47f10caf5ef7f0e395d442aad17e32
jofin-44 <jofin.siby@msds.christuniversity.in>
2024-10-15 13:41:06 +0530

    Admin doctors update

--- FutureGuard/FutureAdmin/views.py
+++ FutureGuard/FutureAdmin/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate, logout
+from .models import Doctor
 
 def admindashboard(request):
     return render(request, 'Admin/MainAdmin/bDbAdmin.html',context={})
@@ -8,10 +9,52 @@ def adminappointments(request):
     return render(request, 'Admin/MainAdmin/bAptAdmin.html',context={})
 
 def admindoctors(request):
-    return render(request, 'Admin/MainAdmin/bDrAdmin.html',context= {})
+    doctors = Doctor.objects.all()
+    
+    context = {
+        "doctors" : doctors
+    }
+    
+    return render(request, 'Admin/MainAdmin/bDrAdmin.html',context)
 
 def adminpatients(request):
     return render(request, 'Admin/MainAdmin/bPtAdmin.html',context={})
 
 def adminsettings(request):
     return render(request, 'Admin/MainAdmin/bSetAdmin.html',context={})
+
+def adminlogout(request):
+    logout(request)
+    return redirect("index")
+
+def add_doctor(request):
+    if request.method == 'POST':
+        name = request.POST.get('doctorName')
+        specialization = request.POST.get('specialization')
+        contact = request.POST.get('contact')
+
+        # Create a new doctor instance and save it to the database
+        doctor = Doctor(name=name, specialization=specialization, contact=contact)
+        doctor.save()
+
+        return redirect('admindoctors')  # Redirect to the doctor page after saving
+
+    return render(request, 'Admin/MainAdmin/bDrAdmin.html')
+
+def edit_doctor(request, doctor_id):
+    doctor = get_object_or_404(Doctor, id=doctor_id)  # Get the doctor by ID
+
+    if request.method == 'POST':
+        doctor.name = request.POST.get('doctorName')  # Update the name
+        doctor.specialization = request.POST.get('specialization')  # Update specialization
+        doctor.contact = request.POST.get('contact')  # Update contact
+        doctor.save()  # Save changes to the database
+
+        return redirect(admindoctors)  # Redirect to the doctor page after saving
+
+    return render(request, 'Admin/MainAdmin/editDoctor.html', {'doctor': doctor})
+
+def delete_doctor(request, doctor_id):
+    dr = Doctor.objects.get(id = doctor_id)
+    dr.delete()
+    return redirect(admindoctors)
